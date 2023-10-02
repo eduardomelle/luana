472fbf3edec89399c6fffcea32b607bebaa78a07
Eduardo Orlandi Melle <eduardomelle@gmail.com>
2023-10-02 07:56:45 -0300

    Implementação da etapa level 3 do projeto.

--- java-challenge/src/main/java/br/com/luanasoares/javachallenge/controller/MovieController.java
+++ java-challenge/src/main/java/br/com/luanasoares/javachallenge/controller/MovieController.java
@@ -3,18 +3,29 @@ package br.com.luanasoares.javachallenge.controller;
 import br.com.luanasoares.javachallenge.dto.MovieDto;
 import br.com.luanasoares.javachallenge.model.Movie;
 import br.com.luanasoares.javachallenge.service.MovieService;
+import com.hazelcast.core.HazelcastInstance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ConcurrentMap;
 
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
 
     public final MovieService movieService;
+
+    @Autowired
+    private HazelcastInstance hazelcastInstance;
+
+    private ConcurrentMap<String,List<MovieDto>> retrieveMap() {
+        return hazelcastInstance.getMap("map");
+    }
 
     public MovieController(MovieService movieService) {
         this.movieService = movieService;
@@ -32,7 +43,14 @@ public class MovieController {
 
     @GetMapping("/top-10")
     public ResponseEntity<List<MovieDto>> findByTop() {
-        return ResponseEntity.ok(this.movieService.findByTop());
+        List<MovieDto> movies = null;
+        if (this.retrieveMap().get("top-10") == null) {
+            movies = this.movieService.findByTop();
+            this.retrieveMap().put("top-10", movies);
+        } else {
+            movies = this.retrieveMap().get("top-10");
+        }
+        return ResponseEntity.ok(movies);
     }
 
 }
